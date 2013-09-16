7a8286f4b14e5b99327df7259ebafbb22578eb45
Denis Krivitski <denis.krivitski@gmail.com>
2013-09-16 10:09:24 +0300

    Common project moved temporarily out. IntelliJ project created

--- CxCommonGeneral/src/com/checkmarx/cxviewer/ws/resolver/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://Checkmarx.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.checkmarx.cxviewer.ws.resolver;
