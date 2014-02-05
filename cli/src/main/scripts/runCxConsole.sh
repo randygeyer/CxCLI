eed06da41e8d13835882f80b1ed8c66521ead6ce
David Kranzdorf <davidkranzdorf@gmail.com>
2014-02-05 15:45:07 +0200

    removed '\r' characters from runCxConsole.sh bash script  + Changed bash direction (bug 11153) - fixed also in version 7.1.2.25

--- cli/src/main/scripts/runCxConsole.sh
+++ cli/src/main/scripts/runCxConsole.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 pushd "`dirname \"$0\"`"
-java -Xmx1024m -cp :/config/cx_console.properties -jar cx_console.jar $@
+java -Xmx1024m -cp :./config/cx_console.properties -jar cx_console.jar $@
 popd
