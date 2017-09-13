8859089d33490e0fe88ea91fe7bd30b4724c4285
nirli <nir.liberman@checkmarx.com>
2017-09-13 09:15:43 +0300

    Added commands skeleton

---
+++ cli/src/main/java/com/checkmarx/v2/commands/OsaScanCommand.java
@@ -0,0 +1,13 @@
+package com.checkmarx.v2.commands;
+
+public class OsaScanCommand extends CommandImpl<> {
+    @Override
+    protected void updateModelState(Object o) {
+
+    }
+
+    @Override
+    protected Object handleExecution() throws Exception {
+        return null;
+    }
+}
--- cli/src/main/java/com/checkmarx/v2/factories/CommandsFactory.java
+++ cli/src/main/java/com/checkmarx/v2/factories/CommandsFactory.java
@@ -7,19 +7,37 @@ import java.util.List;
 
 public class CommandsFactory {
 
-    public List<Command> createPreScanCommands() {
+    public List<Command> createInitializeCLICommands() {
         List<Command> commands = new ArrayList<>();
         commands.add(new GetConfigurationFileCommand());
         commands.add(new GetArgsCommand());
+        commands.add(new LoginCommand());
+
+        return commands;
+    }
+
+
+    public List<Command> createPreScanCommands() {
+        List<Command> commands = new ArrayList<>();
         commands.add(new GetPresetsCommand());
         commands.add(new GetTeamsCommand());
         commands.add(new GetEngineConfigurationsCommand());
 
         return commands;
     }
-//
-//    public Command createValidatedScanCommand() {
-//
-//    }
+
+    public List<Command> createSastScanCommand() {
+        List<Command> commands = new ArrayList<>();
+        commands.add(new SastScanCommand());
+
+        return commands;
+    }
+
+    public List<Command> createOsaScanCommand() {
+        List<Command> commands = new ArrayList<>();
+        commands.add(new OsaScanCommand());
+
+        return commands;
+    }
 
 }
