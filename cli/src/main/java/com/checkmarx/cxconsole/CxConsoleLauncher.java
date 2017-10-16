3ad8a4f5bf2e3fa4d6157f453c84aa8471a686a0
nirli <nir.liberman@checkmarx.com>
2017-10-16 13:15:57 +0300

    Bug fixes: won't print help for report parameters in AsyncScan

--- cli/src/main/java/com/checkmarx/cxconsole/CxConsoleLauncher.java
+++ cli/src/main/java/com/checkmarx/cxconsole/CxConsoleLauncher.java
@@ -3,11 +3,9 @@ package com.checkmarx.cxconsole;
 import com.checkmarx.cxconsole.commands.CommandsFactory;
 import com.checkmarx.cxconsole.commands.CxConsoleCommand;
 import com.checkmarx.cxconsole.utils.BuildVersion;
-import com.checkmarx.cxconsole.utils.CommandLineArgumentException;
 import com.checkmarx.cxconsole.utils.ConfigMgr;
 import com.checkmarx.cxconsole.utils.CustomStringList;
 import com.checkmarx.cxviewer.ws.SSLUtilities;
-import org.apache.commons.cli.ParseException;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -112,17 +110,11 @@ public class CxConsoleLauncher {
                     return errorCodeResolver(e.getMessage());
                 }
                 command.checkParameters();
-            } catch (ParseException e) {
-                log.fatal(INVALID_COMMAND_PARAMETERS_MSG + e.getMessage() + "\n");
-                command.printHelp();
-                return errorCodeResolver(e.getCause().getMessage());
-            } catch (CommandLineArgumentException e) {
-                log.fatal(INVALID_COMMAND_PARAMETERS_MSG + e.getMessage() + "\n");
-                command.printHelp();
-                return errorCodeResolver(e.getMessage());
             } catch (Exception e) {
                 log.fatal(INVALID_COMMAND_PARAMETERS_MSG + e.getMessage() + "\n");
-                command.printHelp();
+                if (!command.getCommandName().toLowerCase().contains("async")) {
+                    command.printHelp();
+                }
                 return errorCodeResolver(e.getMessage());
             }
 
